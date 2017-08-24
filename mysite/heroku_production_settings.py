d1d4b4be2c66646a15f5fcf503aade6b76378146
Wemism <wemisim27@gmail.com>
2017-08-24 20:13:58 +0800

    modify mail backend service sensitive var to environment var

--- mysite/heroku_production_settings.py
+++ mysite/heroku_production_settings.py
@@ -9,7 +9,8 @@ import dj_database_url
 #     default='postgres://localhost')}
 DATABASES = {'default': dj_database_url.config()}
 
-SECRET_KEY = os.environ['SECRET_KEY']  # for deploy
+# SECRET_KEY = os.environ['SECRET_KEY']  # for deploy
+SECRET_KEY = os.environ.get('SECRET_KEY')  # for deploy
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -30,8 +31,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'wmsback@gmail.com'
-EMAIL_HOST_PASSWORD = 'backlight'
+# EMAIL_HOST_USER = 'wmsback@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  # for deploy
+# EMAIL_HOST_PASSWORD = 'uvcirpodxateyhtp'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  # for deploy
 
 LOGGING = {
     'version': 1,
@@ -61,10 +64,12 @@ LOGGING = {
     }
 }
 
-ADMINS = (  # error 500
-    ('admin1', 'wemism27@gmail.com'),
-)
+# ADMINS = (  # error 500
+#     ('admin', 'wemism27@gmail.com'),
+# )
+ADMINS = os.environ.get('ADMINS')  # error 500 mail
 
-MANAGERS = (  # error 404
-    ('manager1', 'wemism27@gmail.com'),
-)
+# MANAGERS = (  # error 404
+#     ('manager1', 'wemism27@gmail.com'),
+# )
+MANAGERS = os.environ.get('MANAGERS')  # error 404 mail
